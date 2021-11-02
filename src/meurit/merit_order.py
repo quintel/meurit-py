8a5cbf9f8959f856817e113762aea0ac640bd573
Nora Schinkel <ncschinkel@gmail.com>
2021-11-02 17:56:24 +0100

    Add a Python wrapper for Merit::Order from the Ruby Merit gem

---
+++ src/meurit/merit_order.py
@@ -0,0 +1,74 @@
+import vendor.rython as rython
+
+merit_context = rython.RubyContext(requires=['bundler/setup', "quintel_merit"], debug=True)
+
+class MeritOrder:
+    '''Sorta wraps the Ruby Merit gem'''
+    def __init__(self):
+        self.merit_order = merit_context("Merit::Order.new")
+
+
+    def add_participant(self, participant='MustRunProducer', **kwargs):
+        '''
+        Adds a participant to the Merit Order (supply).
+
+        See MO docs for all types of participants.
+        '''
+        self._add(merit_context(f"Merit::{participant}.new({convert_to_ruby_hash_string(kwargs)})"))
+
+
+    def add_user(self, **kwargs):
+        '''
+        Adds a User to the Merit order (demand).
+
+        Params:
+            kwargs: Values to pass to the creation of the User in the
+                    Merit gem. See Merit documentation for full info.
+                    Required: key. Choose one of: total_consumption,
+                    load_curve, consumption_share.
+        '''
+        self._add(merit_context(f"Merit::User.create({convert_to_ruby_hash_string(kwargs)})"))
+
+
+    def _add(self, participant):
+        '''Add a Merit::Participant to the merit order'''
+        self.merit_order('add(%(new_participant)r)', new_participant=participant)
+
+
+    def calculate(self):
+        '''Calculates the Merit Order based on the added participants'''
+        self.merit_order('calculate')
+
+
+    def price_curve(self):
+        '''Returns the price curve'''
+        return self.merit_order('price_curve')('to_a')
+
+
+# Helper functions -------------------------------------------------------------
+
+def convert_to_ruby_hash_string(dictionary):
+    '''Converts a Python dict to a Ruby hash syntax in a string'''
+    return ', '.join((convert_to_ruby_key_value_pair(k,v) for k,v in dictionary.items()))
+
+
+def convert_to_ruby_key_value_pair(key, value):
+    '''
+    Converts a Python key value pair into a Ruby syntax key value pair, paying
+    attention to the difference between symbol and string values.
+
+    Returns
+        str: Ruby hash key value pair as a string
+    '''
+    if key == 'load_profile':
+        return f'{key}: {load_profile(value)}'
+
+    if isinstance(value, str) and value[0] != ':':
+        return f"{key}: '{value}'"
+
+    return f'{key}: {value}'
+
+
+def load_profile(path):
+    '''Returns a str version of Ruby code for creating a load profile from a path'''
+    return f"Merit::LoadProfile.load('{path}')"
---
+++ tests/test_merit_order.py
@@ -0,0 +1,56 @@
+import pytest
+
+from meurit.merit_order import MeritOrder
+
+@pytest.fixture
+def must_run_values():
+    return {'key': ':agriculture_chp_engine_network_gas', 'marginal_costs': 81.34086561,
+        'output_capacity_per_unit': 1.01369863, 'number_of_units': 3023.581081, 'availability': 0.97,
+        'fixed_costs_per_unit': 116478.4738, 'fixed_om_costs_per_unit': 13062.47379,
+        'load_profile': 'tests/fixtures/fake_curve.csv',
+        'full_load_hours': 3980.424144}
+
+
+def test_merit_instance():
+    mo = MeritOrder()
+    assert mo.merit_order("to_s") == '<#Merit::Order (0 producers, 0 users, 0 flex, 0 price-sensitives)>'
+
+def test_add_participant(must_run_values):
+    mo = MeritOrder()
+
+    mo.add_participant(participant='MustRunProducer', **must_run_values)
+
+    assert mo.merit_order("to_s")  == '<#Merit::Order (1 producers, 0 users, 0 flex, 0 price-sensitives)>'
+    assert mo.merit_order("participants")("first")("to_s") == f'#<Merit::MustRunProducer {must_run_values["key"][1:]}>'
+    assert mo.merit_order("participants")("first")("full_load_hours") == must_run_values["full_load_hours"]
+
+
+def test_add_user():
+    user_values = {'key': ':total_demand',
+        'load_profile': 'tests/fixtures/fake_curve.csv',
+        'total_consumption': 417946498897.5582}
+
+    mo = MeritOrder()
+
+    mo.add_user(**user_values)
+
+    assert mo.merit_order("to_s")  == '<#Merit::Order (0 producers, 1 users, 0 flex, 0 price-sensitives)>'
+    assert mo.merit_order("participants")("first")("to_s") == f'#<Merit::User::TotalConsumption {user_values["key"][1:]}>'
+
+
+def test_calculate_and_price_curve(must_run_values):
+    mo = MeritOrder()
+
+    # Add demand
+    user_values = {'key': ':total_demand',
+        'load_profile': 'tests/fixtures/fake_curve.csv',
+        'total_consumption': 417946498897.5582}
+    mo.add_user(**user_values)
+
+    # Add supply
+    mo.add_participant(participant='MustRunProducer', **must_run_values)
+
+    mo.calculate()
+    pc = mo.price_curve()
+    assert isinstance(pc, list)
+    assert len(pc) == 8760

9bd5a4580ee779acbe0f55248e054c5639190369
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2021-11-22 09:52:09 +0100

    Setup experiment

--- experiments/bottom_up.py
+++ experiments/bottom_up.py
@@ -3,22 +3,51 @@
 from lib.models import Country, InactiveCountry, Area
 from lib.logger import DataLogger
 
+path = '~/Dropbox (Quintel)/Quintel/Projects/Active/538 Coupled Markets'
+
 # Setup the coutries and put them in an Area for enabling 'all' methods
 nl = Country('nl')
 de = Country('de')
 be = Country('be')
+dk = Country('dk')
+at = Country('at')
+fr = Country('fr')
+no = Country('no')
+se = Country('se')
+uk = Country('uk')
+lu = Country('lu')
+ch = Country('ch')
+oth = Country('oth')
 
 # Add an inactive coutry
-dk = InactiveCountry.from_price_curve_file('dk', 'experiments/price_curves/curve.csv')
+at = InactiveCountry.from_price_curve_file('vertrouwelijk - at 2015', path)
+fr = InactiveCountry.from_price_curve_file('vertrouwelijk - fr 2015', path)
+no = InactiveCountry.from_price_curve_file('vertrouwelijk - no 2015', path)
+se = InactiveCountry.from_price_curve_file('vertrouwelijk - se 2015', path)
+uk = InactiveCountry.from_price_curve_file('vertrouwelijk - uk 2015', path)
+lu = InactiveCountry.from_price_curve_file('vertrouwelijk - oth 2015', path)
+ch = InactiveCountry.from_price_curve_file('vertrouwelijk - oth 2015', path)
+oth = InactiveCountry.from_price_curve_file('vertrouwelijk - oth 2015', path)
 
-area = Area(nl, be, de, dk)
+area = Area(nl, be, de, dk, at, fr, no, se, uk, lu, su, ch, oth)
 
 # Add interconnectors and capacities
-nl.build_interconnector_to(de, 3000)
-nl.build_interconnector_to(be, 3000)
-de.build_interconnector_to(be, 3000)
-de.build_interconnector_to(dk, 3000) # Static interconnector
+be.build_interconnector_to(lu, 680)    # static
+be.build_interconnector_to(de, 1000)   # dynamic
+be.build_interconnector_to(fr, 1800)   # static
+be.build_interconnector_to(nl, 2400)   # dynamic
 
+de.build_interconnector_to(oth, 10115) # static
+de.build_interconnector_to(be, 1000)   # dynamic
+de.build_interconnector_to(dk, 2500)   # dynamic
+de.build_interconnector_to(ch, 2700)   # static
+de.build_interconnector_to(nl, 4250)   # dynamic
+de.build_interconnector_to(at, 5400)   # static
+
+nl.build_interconnector_to(no, 700)    # static
+nl.build_interconnector_to(uk, 1000)   # static
+nl.build_interconnector_to(be, 1400)   # dynamic
+nl.build_interconnector_to(de, 4250)   # dynamic
 
 # Let's call the ETM to calculate some price-curves and volumes for us
 iterations = 2
